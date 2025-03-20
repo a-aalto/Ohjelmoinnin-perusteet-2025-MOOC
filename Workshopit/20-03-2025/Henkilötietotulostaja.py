1315117637007aa4e0c26274507f33524d9afee5
Antti Aalto <antti.johannes.aalto@student.laurea.fi>
2025-03-20 10:25:18 +0200

    Lisätään Workshopit-hakemisto

---
+++ Workshopit/20-03-2025/Henkilötietotulostaja.py
@@ -0,0 +1,41 @@
+#Harjoitus 1
+
+print("Henkilötietotulostaja\n")
+print("Syötä henkilötiedot:")
+print("------------------------\n")
+
+etunimet = input("Anna etunimet:")
+sukunimi = input("Anna sukunimi:")
+puhnro = input("Anna puhelinnumerosi:")
+email = input("Anna sähköpostiosoitteesi:")
+osoite = input("Anna lähiosoitteesi:")
+postinumero = input("Anna postinumerosi:")
+kaupunki = input("Anna kaupunki:")
+maa = input("Anna maa:")
+syntymävuosi = input("Anna syntymävuosi:")
+print()
+
+print ("KIITOS!\n\n")
+
+print("HENKILÖN TIEDOT")
+print("------------------")
+print()
+print("NIMI:")
+print("  "+sukunimi+", "+etunimet)
+print("PUH:")
+print("  "+puhnro)
+print("EMAIL:")
+print("  "+email)
+print("OSOITE:")
+print("  "+osoite)
+print("  "+postinumero+" "+kaupunki)
+print("  "+maa)
+
+
+
+
+
+
+
+
+
